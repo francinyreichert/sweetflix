46b749ae859b2c7a753f5a019b8db4e05f0d84eb
FrancinyReichert <franciny-machado@hotmail.com>
2020-07-30 20:28:04 -0300

    ultimas atualizacoes aula 4

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -32,7 +32,9 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if(window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias'; 
+      const URL = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://devfranflix.herokuapp.com/'; 
       fetch(URL)
        .then(async (respostaDoServer) =>{
         if(respostaDoServer.ok) {
